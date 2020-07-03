202b95533f6f67d174dd7ba4d8926937919d18ef
nkochelev <67287904+nkochelev@users.noreply.github.com>
2020-07-03 17:28:30 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@ def validate_post_data(data: dict) -> bool:
 
 @app.route('/', methods=['GET'])
 def hello():
-    return 'Hello World1234!'
+    return 'Hello World12345!'
     
 @app.route('/api', methods=['GET', 'POST'])
 def api():
